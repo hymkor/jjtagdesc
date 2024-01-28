89763f54c0c86dd64c279ad4a1fd9dcf427ef69e
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-01-29 08:12:50 +0900

    Add main script Add default files: .gitattributes and LICENSE

---
+++ tagdesc.ps1
@@ -0,0 +1,19 @@
+Set-PSDebug -Strict
+$count = 0
+jj log --no-graph -r "latest(tags())::" | ForEach-Object {
+    $count++
+    if ( $count % 2 -ne 0 ){
+        $fields = ($_ -split " +")
+        if ( $count -eq 1 ){
+            $current = $fields[0]
+        }
+        if ( $fields.Length -eq 7 ){
+            if ( $count -eq 1 ){
+                $result = $fields[5]
+            } else {
+                $result = ("{0}-{1}-{2}" -f $fields[5],$count,$current)
+            }
+        }
+    }
+}
+Write-Output $result
