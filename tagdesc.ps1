bf2b16bf58f869e07cd75e8fd483f2bb9b186af8
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-01-29 10:09:01 +0900

    tagdesc.ps1: ignore no description commits

--- tagdesc.ps1
+++ tagdesc.ps1
@@ -1,9 +1,9 @@
 Set-PSDebug -Strict
 $count = 0
 $result = $null
-jj log --no-graph -r "latest(tags())::" | ForEach-Object {
     $count++
     if ( $count % 2 -ne 0 ){
+jj log --no-graph -r 'latest(tags()):: ~ description(exact:"")' | ForEach-Object {
         $fields = ($_ -split " +")
         if ( $count -eq 1 ){
             $current = $fields[0]
@@ -15,6 +15,7 @@ jj log --no-graph -r "latest(tags())::" | ForEach-Object {
                 $result = ("{0}-{1}-{2}" -f $fields[5],$count,$current)
             }
         }
+        $ncommits++
     }
 }
 if ( -not $result ){
